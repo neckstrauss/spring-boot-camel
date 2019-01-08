523ee5e5945e8fc56c6626744328345bb3516a4f
USER <USER@DESKTOP-HLCIB5D>
2019-01-08 17:26:43 -0500

    prueba

--- src/main/java/io/fabric8/quickstarts/camel/Application.java
+++ src/main/java/io/fabric8/quickstarts/camel/Application.java
@@ -21,21 +21,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 /**
- * A spring-boot application that includes a Camel route builder to setup the Camel routes
+ * A spring-boot application that includes a Camel route builder to setup the
+ * Camel routes
  */
 @SpringBootApplication
-@ImportResource({"classpath:spring/camel-context.xml"})
+@ImportResource({ "classpath:spring/camel-context.xml" })
 public class Application extends RouteBuilder {
 
-    // must have a main method spring-boot can run
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	// must have a main method spring-boot can run
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
-    @Override
-    public void configure() throws Exception {
-        from("timer://foo?period=5000")
-            .setBody().constant("Hello World")
-            .log(">>> ${body}");
-    }
+	@Override
+	public void configure() throws Exception {
+//        from("timer://foo?period=5000")
+//            .setBody().constant("Hello World")
+//            .log(">>> ${body}");
+
+		rest("/").produces("text/plain")
+			.get("hello")
+			.to("direct:hello");
+
+		from("direct:hello")
+				// .to("geocoder:address:current")
+				.transform().simple("Hello from Spring Boot and Camel. We are at: ");
+	}
 }
